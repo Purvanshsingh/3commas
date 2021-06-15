15bb3811aa51d97a68d43103b61cc454d921ac58
Purvansh Singh <purvanshsingh@gmail.com>
2021-06-15 06:20:25 +0530

    Update UpdateBots.py

--- UpdateBots.py
+++ UpdateBots.py
@@ -11,10 +11,8 @@ class UpdateBots:
     def __init__(self):
         self.db_object = MongoConnection.App_mongo_connect()
         # API key & Secret Key
-        self.key = '3e45e0cb611845a19bc053f4752090704b20f1085c2d4951b4606b99d11db935'
-        self.secret = 'a0dc20cb712001b1bad8eb82d1e453b5b70042897d417fc4d3ba84efb734190d30759bd61' \
-                      'c69df14032ddb1ee3eafbb923e95683c4114e65a35fee54e0420d2ad5902a5432c266273d7f7cd' \
-                      'f3e07285b8ecdf5e431679b1c0a7a8f76b51c5b77a20aaac7'
+        self.key = 'API_Key to replaced here'
+        self.secret = 'Secret_key to be replaced here'
         # py3wc Object
         self.py3cw = Py3CW(self.key,self.secret)
         # Argument input_file
